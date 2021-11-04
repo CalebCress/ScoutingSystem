58971a29999d4af4a756e3155b707cfbf053b956
calebc <calebc1@protonmail.com>
2021-11-04 13:27:40 -0700

    began mongo implementation

--- server/index.js
+++ server/index.js
@@ -1,19 +1,46 @@
-import express from 'express'
-import mongodb from 'mongodb'
-
-
 const express = require('express')
+const { MongoClient } = require('mongodb')
+
+
+/****************************/
+/*         Database         */
+/****************************/
+// Connection URI
+const uri =
+  "mongodb+srv://localhost:27017/scouting" //uses scouting database
+
+// Create a new MongoClient
+const client = new MongoClient(uri)
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect()
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 })
+    console.log("Connected successfully to server")
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+/****************************/
+/*           API            */
+/****************************/
+
 const app = express()
 const port = 3000
 
 app.post('/submit', (req, res) => {
-  res.send(200)
+    
+    res.send(200)
 })
 
 app.get('/', (req, res) => {
     res.send('scouting system root')
 })
 
-app.listen(4444, () => {
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
--- specsheet.md
+++ specsheet.md
@@ -1,8 +1,15 @@
 # Specsheet
-## API Endpoints
-### Submit Scouting Data
+Specsheet for team1540 Scouting System
+## API
+### API Endpoints
+#### Submit Scouting Data
 - `Endpoint`: `/submit`
 - `Fields`:
   - `data`: json containing scouting data
   - `scout`: name of the scout who collected the data
   - `teamNumber`: team scouteds number
+
+## Database
+Uses MongoDB with database named `scouting`.
+### Database Scheme
+
