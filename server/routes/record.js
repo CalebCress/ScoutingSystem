e8111732ff7923318c912dbb98b94a5c9b67f3db
calebc <calebc1@protonmail.com>
2022-01-13 19:21:54 -0800

    created /submit api route

--- server/routes/record.js
+++ server/routes/record.js
@@ -5,3 +5,23 @@ const express = require("express");
 // The router will be added as a middleware and will take control of requests starting with path /listings.
 const recordRoutes = express.Router();
 
+recordRoutes.route("/submit").get(async function (req, res) {
+    const dbConnect = dbo.getDb();
+    let id
+
+    dbConnect.collection("teams").findOne({number: req.body.teamNumber}, (err, result) => {
+        if (err) throw err;
+        id = result.id
+    })
+    let data = {
+        teamId: id,
+        scout: res.scout,
+        data: res.data,
+        eventId: res.eventId
+    }
+    dbConnect.collection("data").insertOne(data, (err, res) => {
+        if (err) throw err;
+        console.log("1 document inserted")
+        db.close()
+    })
+})
