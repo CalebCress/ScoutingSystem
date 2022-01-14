58e59bcc9841f4de7fa86981ef9ae9b50ced2618
calebc <calebc1@protonmail.com>
2022-01-14 07:05:47 -0800

    added /addteam route

--- server/routes/record.js
+++ server/routes/record.js
@@ -5,18 +5,42 @@ const express = require("express");
 // The router will be added as a middleware and will take control of requests starting with path /listings.
 const recordRoutes = express.Router();
 const dbo = require('../db/conn');
+const dbConnect = dbo.getDb();
 
-
-recordRoutes.route("/submit").get(async function (req, res) {
-    const dbConnect = dbo.getDb();
-    dbConnect.collection("data").insertOne(req.body, (err, response) => {
+recordRoutes.route("/submit").post(async (req, res) => {
+    let data = {
+        teamNumber: req.body.teamNumber,
+        scout: req.body.scout,
+        data: req.body.data,
+        eventId: req.body.eventId
+    }
+    dbConnect.collection("data").insertOne(data, (err, response) => {
         if (err) {
             res.status(400).send({
                 err,
                 message: "failed to submit data"
             })
         }
-        db.close()
+    })
+})
+
+recordRoutes.route("/addteam").post(async (req, res) => {
+    if (req.body.number === nil || req.body.name === nil) {
+        res.status(400).send({
+            message: "must include number and name in body"
+        })
+    }
+    let team = {
+        number: req.body.number,
+        name: req.body.name
+    }
+    dbConnect.collection("teams").insertOne(team, (err, response) => {
+        if (err) {
+            res.status(400).send({
+                err,
+                message: "failed to add team"
+            })
+        }
     })
 })
 
